e3c6cf07403411642adf12fea9d8d17d24dbaae7
poibms <easypeasylemonskvii@gmail.com>
2025-02-17 10:54:35 +0100

    init

---
+++ app/mealplan/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MealPlanDashboard = () => {
+  return (
+    <div>
+      Meal Plan Page
+    </div>
+  );
+};
+
+export default MealPlanDashboard;
---
+++ middleware.ts
@@ -0,0 +1,34 @@
+import {clerkMiddleware, createRouteMatcher} from "@clerk/nextjs/server";
+import {NextResponse} from "next/server";
+
+const publicRoutes = createRouteMatcher([
+  '/',
+  '/sign-up(.*)',
+  '/subscribe(.*)',
+])
+
+const isSignUpRoute = createRouteMatcher(['/sign-up(.*)',])
+
+export default clerkMiddleware(async (auth, req) => {
+  const {userId} = await auth();
+  const {origin} = req.nextUrl;
+
+  if(!publicRoutes(req) && !userId) {
+    return NextResponse.redirect(new URL('/sign-up', origin));
+  }
+
+  if(isSignUpRoute(req) && userId) {
+    return NextResponse.redirect(new URL('/mealplan', origin));
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
